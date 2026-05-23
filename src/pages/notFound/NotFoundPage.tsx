4d0bf839c1fde0bf47d89473a16869e28a070c35
Karol Nowak <karnowa88@gmail.com>
2026-05-23 10:52:26 +0200

    favicon added

--- src/pages/notFound/NotFoundPage.tsx
+++ src/pages/notFound/NotFoundPage.tsx
@@ -1,20 +1,42 @@
+import { useNavigate } from 'react-router-dom';
 import { Button, Container, Group, Text, Title } from '@mantine/core';
 import Illustration from './components/Ilustration';
 import classes from './NotFound.module.css';
 
-export default function NotFoundPage(){
-    return ( <Container className={classes.root}>
+export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  return (
+    <Container className={classes.root}>
       <div className={classes.inner}>
         <Illustration className={classes.image} />
         <div className={classes.content}>
-          <Title className={classes.title}>You have found a secret place.</Title>
-          <Text c="dimmed" size="lg" ta="center" className={classes.description}>
-           Unfortunately, this is only a 404 page which means the one you are looking for does not exist.
+          <Title className={classes.title} fw="bold" c="var(--mantine-color-gray-9)">
+            You have found a secret place.
+          </Title>
+          <Text
+            c="var(--mantine-color-gray-7)"
+            size="lg"
+            ta="center"
+            className={classes.description}
+          >
+            Unfortunately, this is only a 404 page which means the one you are looking for does not
+            exist.
           </Text>
           <Group justify="center">
-            <Button size="md" m='lg'variant='outline'>Take me back to home page</Button>
+            <Button
+              size="md"
+              m="lg"
+              variant="outline"
+              onClick={() => {
+                navigate('/dashboard');
+              }}
+            >
+              Take me back to home page
+            </Button>
           </Group>
         </div>
       </div>
-    </Container>)
+    </Container>
+  );
 }
